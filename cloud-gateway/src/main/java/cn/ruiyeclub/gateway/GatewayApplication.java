8a21e0e764093790e599bd0459cb14811fc6f7cd
ruiyeclub <ruiyeclub@foxmail.com>
2020-08-25 09:22:59 +0800

    8/25

--- cloud-gateway/src/main/java/cn/ruiyeclub/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/cn/ruiyeclub/gateway/GatewayApplication.java
@@ -2,14 +2,14 @@ package cn.ruiyeclub.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Ray。
  * @date 2020/8/23 17:42
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient //被Eureka注册
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
