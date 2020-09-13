136addc969cba280f81ca7a1b59a2b7ad305ed61
ruiyeclub <ruiyeclub@foxmail.com>
2020-09-13 20:36:08 +0800

    config

--- user-service/src/main/java/cn/ruiyeclub/user/UserApplication.java
+++ user-service/src/main/java/cn/ruiyeclub/user/UserApplication.java
@@ -2,7 +2,6 @@ package cn.ruiyeclub.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -10,7 +9,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @author Ray。
  * @date 2020/8/8 18:25
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @MapperScan("cn.ruiyeclub.user.mapper")
 @EnableDiscoveryClient
 public class UserApplication {
--- user-service/src/main/java/cn/ruiyeclub/user/controller/UserController.java
+++ user-service/src/main/java/cn/ruiyeclub/user/controller/UserController.java
@@ -2,6 +2,7 @@ package cn.ruiyeclub.user.controller;
 
 import cn.ruiyeclub.user.entity.User;
 import cn.ruiyeclub.user.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ public class UserController {
 
     @Resource
     private UserService userService;
+    @Value("${config.test}")
+    private String test;
 
     @GetMapping("/{id}")
     public User queryById(@PathVariable Long id){
@@ -24,5 +27,10 @@ public class UserController {
             e.printStackTrace();
         }*/
         return userService.queryById(id);
+    }
+
+    @GetMapping("test")
+    public String test(){
+        return test;
     }
 }
