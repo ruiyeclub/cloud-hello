ec09a67e6cdce04e0c723e75009c1d47495a1101
ruiyeclub <ruiyeclub@foxmail.com>
2020-09-13 18:48:35 +0800

    config

--- user-service/src/main/java/cn/ruiyeclub/user/UserApplication.java
+++ user-service/src/main/java/cn/ruiyeclub/user/UserApplication.java
@@ -2,6 +2,7 @@ package cn.ruiyeclub.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -9,7 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @author Ray。
  * @date 2020/8/8 18:25
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("cn.ruiyeclub.user.mapper")
 @EnableDiscoveryClient
 public class UserApplication {
